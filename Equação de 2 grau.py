34715e37ea592250c89ae71c54d7c04a907eff7a
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-05-28 17:13:41 -0300

    Add files via upload

---
+++ Equação de 2 grau.py
@@ -0,0 +1,10 @@
+
+a=int(input('digite o valor de a:'))
+b=int(input('Digite o valor de b: '))
+c=int(input('Digite o valor de c: '))
+delta= b**2 - 4*a*c
+print(' o valor de delta é:',delta)
+x1 = (-b - delta**0.5) / (2*a)
+x2 = (-b + delta**0.5) / (2*a)
+print ("o valor de x1 e: ",x1)
+print('O valor de x2 e: ',x2)
