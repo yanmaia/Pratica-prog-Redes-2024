a3c2967fec6dd1ba2c66e5a905b58f01bf010f9d
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-07-09 17:26:04 -0300

    Add files via upload

---
+++ mostragem de lista voltando.py
@@ -0,0 +1,10 @@
+num= int(input('Informe um numero inteiro: '))
+for i in range (num + 1):      # mostra
+    for j in range (i+1):
+        print(j,end=' ')  # end= faz a junçao do print seguinte pelo espaço designado, enquanto j esta printando o valor de i e j
+    print()
+
+for i in range (num):
+    for j in range (num-i):
+        print(j,end=' ')  # end= faz a junçao do print seguinte pelo espaço designado, enquanto j esta printando o valor de i e j
+    print()
