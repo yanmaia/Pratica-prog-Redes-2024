929e94b54db003e9290999064bff8ac00b59c686
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-07-29 17:13:34 -0300

    Update transf numero bynario.py

--- transf numero bynario.py
+++ transf numero bynario.py
@@ -1,7 +1,7 @@
 num=int(input("Informe um valor inteiro para transformarmos em binario."))
-bin=[ ]
+bin=''
 while num>0:
     resto=  num%2
-    bin+=[resto]
+    bin+=str(resto)
     num//=2
-print( "o numero binario e:",bin[::-1])
+print( "o numero binario e:",bin)
